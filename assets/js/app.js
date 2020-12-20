b380d00dd7bf9d37ab82558c8a66f6183243bfae
willywin99 <willywinsianturi@gmail.com>
2020-12-21 04:00:10 +0700

    Directive v-on for key modifier

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,20 @@ var app = new Vue({
     message: '',
   },
   methods: {
-    showAlert: function() {
-      alert('The button has been clicked to call this method!')
-    }
+    escKey: function() {
+      this.message = 'you pressing esc key'
+    },
+    spaceKey: function() {
+      this.message = 'you pressing space key'
+    },
+    upKey: function() {
+      this.message = 'you pressing up key'
+    },
+    downKey: function() {
+      this.message = 'you pressing down key'
+    },
+    aKey: function() {
+      this.message = "you pressing alphabet 'a' key"
+    },
   }
 })
