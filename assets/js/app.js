572087d04569c452071a1c44f253b240f54b6d23
willywin99 <willywinsianturi@gmail.com>
2020-12-21 02:48:38 +0700

    Method Properties

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,11 +5,17 @@ var app = new Vue({
     message: 'Hello World!',
     message2: 'Hello Vue!',
     int1: 1,
-    int2: 2
+    int2: 2,
+    result: null,
   },
   computed: {
     sum: function() {
       return this.int1 + this.int2;
+    }
+  },
+  methods: {
+    sumProcess: function(int3) {
+      return this.result = this.int1 + this.int2 + int3;
     }
   }
 })
