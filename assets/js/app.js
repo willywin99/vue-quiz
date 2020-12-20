95e4197fcef54ea3a5e11e79261c66c11842e2e3
willywin99 <willywinsianturi@gmail.com>
2020-12-21 02:55:41 +0700

    Watch Properties

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,8 @@ var app = new Vue({
     int1: 1,
     int2: 2,
     result: null,
+    kilometers: 0,
+    meters: 0,
   },
   computed: {
     sum: function() {
@@ -16,6 +18,16 @@ var app = new Vue({
   methods: {
     sumProcess: function(int3) {
       return this.result = this.int1 + this.int2 + int3;
+    }
+  },
+  watch: {
+    kilometers: function (val) {
+      this.kilometers = val;
+      this.meters = val * 1000;
+    },
+    meters: function (val) {
+      this.kilometers = val/1000;
+      this.meters = val;
     }
   }
 })
