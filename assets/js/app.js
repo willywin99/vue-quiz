de82e821e4a633f772106407456ba64b41d47e1c
willywin99 <willywinsianturi@gmail.com>
2020-12-21 02:41:06 +0700

    Computed Properties

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,13 @@ var app = new Vue({
   data: {
     show: false,
     message: 'Hello World!',
-    message2: 'Hello Vue!'
+    message2: 'Hello Vue!',
+    int1: 1,
+    int2: 2
+  },
+  computed: {
+    sum: function() {
+      return this.int1 + this.int2;
+    }
   }
 })
