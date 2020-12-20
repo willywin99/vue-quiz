4776a6cc80c8455e0f83d20e0fb9d206a1d6fa8c
willywin99 <willywinsianturi@gmail.com>
2020-12-21 03:02:39 +0700

    Directive v-model in textbox

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    show: false,
-    message: 'Hello World!',
-    message2: 'Hello Vue!',
-    int1: 1,
-    int2: 2,
-    result: null,
-    kilometers: 0,
-    meters: 0,
+    message: ''
   },
-  computed: {
-    sum: function() {
-      return this.int1 + this.int2;
-    }
-  },
-  methods: {
-    sumProcess: function(int3) {
-      return this.result = this.int1 + this.int2 + int3;
-    }
-  },
-  watch: {
-    kilometers: function (val) {
-      this.kilometers = val;
-      this.meters = val * 1000;
-    },
-    meters: function (val) {
-      this.kilometers = val/1000;
-      this.meters = val;
-    }
-  }
+  
 })
