a95879c251c82f04f644b260cb2be679e86be114
willywin99 <willywinsianturi@gmail.com>
2020-12-21 08:24:58 +0700

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,33 @@
-Vue.component('blog-post', {
-  props: ['post'],
-  template: `
-    <div>
-      <h3> {{ post.title }} </h3>
-      <div v-html="post.content"></div>
-    </div>
-  `
-});
+let pos = 0;
 
 var app = new Vue({
   el: '#app',
   data: {
-    posts: [
-      { id: 1, title: 'My journey with Vue', content: 'This is Content of <b>Post 1</b>' },
-      { id: 2, title: 'Blogging with Vue', content: 'This is Content of <b>Post 2</b>' },
-      { id: 3, title: 'Why Vue is so fun', content: 'This is Content of <b>Post 3</b>' },
-    ]
+    content: '',
+    names: ['Muhammad Iqbal Mubarok', 'Faqih Muhammad', 'Rayhan Murtaza'],
+    editStatus: false,
+    // pos: 0,
+  },
+  methods: {
+    updateName: function () {
+      // console.log(pos);
+      // console.log(this.names[pos]);
+      // console.log(this.content);
+      this.names[pos] = this.content;
+      this.editStatus = false;
+    },
+    editName: function (index) {
+      this.content = this.names[index];
+      this.editStatus = true;
+      pos = index;
+      // console.log(pos)
+    },
+    addName: function () {
+      this.names.push(this.content);
+      this.content = '';
+    },
+    removeName: function (index) {
+      this.names.splice(index, 1);
+    }
   }
-});
+}) 
